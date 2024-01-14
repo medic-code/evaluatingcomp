c6e3f9569b8d3fcfe36328037f353ab9ffcc68c6
medic-code <u40as7@gmail.com>
2024-01-14 19:14:16 +0000

    Add basic prompt

---
+++ llm/prompts.js
@@ -0,0 +1,21 @@
+const {PromptTemplate }= require('langchain/prompts');
+
+const QUESTION_TEMPLATE = `You are an experienced researcher, 
+expert at interpreting and answering questions based on provided sources.
+Using the provided context, answer the user's question 
+to the best of your ability using only the resources provided. 
+Be verbose!
+
+<context>
+
+{context}
+
+</context>
+
+Now, answer this question using the above context:
+
+{question}`;
+
+const prompt = PromptTemplate.fromTemplate(QUESTION_TEMPLATE);
+
+module.exports = prompt;
