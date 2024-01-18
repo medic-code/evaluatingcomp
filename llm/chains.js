f45eb12599b461d6ceb30e884ca1388dbeb5c125
medic-code <u40as7@gmail.com>
2024-01-18 21:38:50 +0000

    Fix product string not being interpolated into question

--- llm/chains.js
+++ llm/chains.js
@@ -11,28 +11,28 @@ dotenv.config({ path: `${__dirname}/.env` });
 
 const model = new OpenAI({temperature: 0.9, openAIApiKey:process.env.OPENAI_API_KEY});
 
-async function documentRetrivalChain() {
-    const vectorStore = await createRetriver();
+async function documentRetrivalChain(vectorStore) {
     const retriever = vectorStore.asRetriever();
     const chain = RunnableSequence.from([
-        (input) => input.question,
+        (input) => input.question.replaceAll('{product}',input.product),
         retriever,
         docAsString
     ])
+
     return chain;
 }
 
-async function retrivalChain(question,product) {
+async function retrivalChain(question,product,vectorStore) {
     const chain = RunnableSequence.from([
         {
-            question: (input) => input.question,
-            product: (input) => input.product,
-            context: (input) => documentRetrivalChain(input)
+            question: (input) => input.question.replaceAll(' ', '').replaceAll('{product}',input.product),
+            context: () => documentRetrivalChain(vectorStore)
         },
         prompt,
         model,
         new StringOutputParser()
     ])
+
     const results = await chain.invoke({question: question, product: product });
 
     return results;
