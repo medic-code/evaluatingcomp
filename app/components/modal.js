e454abd036181e31c1e9dcb41adfd1858b2ca314
medic-code <u40as7@gmail.com>
2024-02-14 20:28:29 +0000

    Fix modal styles

--- app/components/modal.js
+++ app/components/modal.js
@@ -4,6 +4,8 @@ import { useState, useEffect } from 'react';
 
 const Modal = ({modal, setModal,user}) => {
 
+    const [show,setShow] = useState(false);
+
     const [key, setKey] = useState('');
     useEffect(() => {
         async function getSecret () {
@@ -35,6 +37,7 @@ const Modal = ({modal, setModal,user}) => {
         } else {
             password.type = 'password';
         }
+        setShow(true);
     }
 
     const setEdit = (event) => {
@@ -69,6 +72,18 @@ const Modal = ({modal, setModal,user}) => {
                 console.error('error inserting data',error)
             }
         } else {
+            if (key === '') {
+                const supabase = createClientComponentClient();
+        const { error } = await supabase.rpc('delete_secret', {
+            secret_name: `${user}`
+        })
+
+
+        if (error) {
+            console.error('Error deleting name', error)
+        }
+
+            }
             const supabase = createClientComponentClient();
             const apikey = await supabase.rpc('read_secretid', {secret_name: `${user}`});
             const { error } = await supabase.rpc('update_secret', {secretid: apikey.data, secret: `${key}`})
@@ -76,10 +91,6 @@ const Modal = ({modal, setModal,user}) => {
                 console.error('error inserting data',error)
             }
         }
-        
-
-       const hiddenInput = document.getElementById('api')
-       hiddenInput.disabled = true;
     }
     return (
         <>
@@ -88,28 +99,42 @@ const Modal = ({modal, setModal,user}) => {
             <div className={styles.modalBackground}>
                 <div className={styles.modalContainer}>
                     <div className={styles.modalHeader}>
-                        <a onClick={() => setModal(false)}>X</a>
+                        <a onClick={() => setModal(false)}><svg width="24" height="24" class="text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 18 6m0 12L6 6"/>
+</svg>
+</a>
                     </div>
                
-                    <h1 className={styles.modalTitle}>Settings</h1>
+                    <h2 className={styles.modalTitle}>Settings</h2>
                  
                     <div className={styles.modalBody}>
                         <form>
-                            
+                        <div className={styles.apikey}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M12 4a4 4 0 0 0-4 4h8a4 4 0 0 0-4-4Zm-6 6a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-8a1 1 0 0 0-1-1H6Zm0-2a6 6 0 1 1 12 0 3 3 0 0 1 3 3v8a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3v-8a3 3 0 0 1 3-3Zm4 6a2 2 0 1 1 3 1.732V17a1 1 0 1 1-2 0v-1.268A2 2 0 0 1 10 14Z" clip-rule="evenodd"></path></svg>
                             <label className={styles.form__label}>
-                                ApiKey
+                                API Key
                             </label>
-                            <div>
-                            <input id='api' type="password" className={styles.form__textarea} value={key}  onChange={(e) => setKey(e.target.value)}/>
                             </div>
-                            <button id='reveal' onClick={showPassword}>Show</button>
-                            <button id='edit' onClick={setEdit}>Edit</button>
+                            <div className={styles.apikey} >
+                                <input id='api' type="password" className={styles.modal__text} value={key}  onChange={(e) => setKey(e.target.value)}/>
+                               <div id='reveal' onClick={showPassword}>
+                                
+                                    <svg width="24" height="24" class="text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                            <path stroke="currentColor" stroke-width="2" d="M21 12c0 1.2-4 6-9 6s-9-4.8-9-6c0-1.2 4-6 9-6s9 4.8 9 6Z"/>
+                                            <path stroke="currentColor" stroke-width="2" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/> </svg>
+                                          
+                                        
+                                       
+
+                                    </div>
+                            </div>
+                        
                         </form>
                     </div>
                     <div className={styles.modalFooter}>
                         <button className={styles.modal__button} onClick={() => setModal(false)}>Cancel</button>
                         <button className={styles.modal__buttonContinue} onClick={handleSave}>Save</button>
-                        <button className={styles.modal__buttonContinue} onClick={handleDelete}>Delete</button>
+                   
 
                     </div>
                 </div>
