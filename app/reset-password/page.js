f9333905f4f1ed18ef2a1762399580b54e33e05a
medic-code <u40as7@gmail.com>
2024-02-15 20:41:02 +0000

    Fix import

--- app/reset-password/page.js
+++ app/reset-password/page.js
@@ -2,7 +2,7 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
-import ResetPassword from '../components/Auth/ResetPassword'
+import ResetPassword from '../components/Auth/resetPassword'
  
 export default async function ResetPasswordPage() {
     const supabase = createServerComponentClient({cookies});
