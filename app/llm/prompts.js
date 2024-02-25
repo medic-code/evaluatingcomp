ad02e655e6647eea7cedf80858490ca48ff66275
medic-code <u40as7@gmail.com>
2024-02-25 11:29:33 +0000

    Add prompt updates and minor fixes

--- app/llm/prompts.js
+++ app/llm/prompts.js
@@ -4,13 +4,18 @@ const QUESTION_TEMPLATE = `You are an experienced researcher,
 expert at interpreting and answering questions based on provided sources.
 Using the provided context, answer the user's question 
 to the best of your ability using only the resources provided. 
-Be verbose!
 
 <context>
 
 {context}
 
 </context>
+
+Please restrict yourself to no more than 200 words as an answer. 
+Please be precise in your answers as well. I want you to be concise and comprehensive. Be creative but not vague in your answers.
+Please avoid referring to context provided in any circumstances
+
+Any bullet points you generate can you please remove any '**' that you generate.
 
 Now, answer this question using the above context:
 
--- app/utils/framework.js
+++ app/utils/framework.js
@@ -2,12 +2,12 @@ export default {
     summary: 'What does {product}? Give a description of what it does and why it exists',
     target_users: "Who is {product} intended for? These groups of people are called target users. Target users are people who have either already adopted {product}, or who {product} is intended for. If this information wasn't provided, list some plausible target users of {product}.",
     problems: "What are the reasons for target users to seek out, and adopt, {product}? These reasons are usually problems or pain points the target users are experiencing. Reasons can also be the needs and desires of target users. If this information wasn't provided, list some plausible reasons for target users to seek out and adopt {product}. You don't need to explain how {product} addresses these reasons yet, that will come next.",
-    solutions: "How does {product} address each one of these reasons? In other words, how does {product} solve user problems, assuage user pain points, or fulfill user needs and desires? If this information wasn't provided, list the plausible ways {product} could address each one of these reasons.",
+    solutions: "How does {product} address each one of these reasons? In other words, how does {product} solve user problems, assuage user pain points, or fulfill user needs and desires? If this information wasn't provided, list the plausible ways {product} could address each one of these reasons. If there are specific products available please name them and provide a description of what each product does.",
     unfair_advantage: "What makes {product} difficult to compete with? These are {product}'s competitive advantages, or the barriers to entry faced by its competitors. If this information wasn't provided, list some plausible competitive advantages and barriers to entry.",
     unique_value_proposition: "What makes {product} unique or special compared to others? This should be a 2-3 sentence unique value proposition.",
     revenue: "How does {product} generate revenue? Give a description of plausible business models and revenue streams for {product}.",
     opportunities: "What are the opportunities for {product}. In other words given the products unfair advantage what are the opportunities for {product} to make money",
     risk: "What are the risks for {product}. In other words what are the risks to the business that means the business might fail.",
     market_size: 'What is the market size of {product}, in other words what is the total addressable market for {product}',
-    competitors: 'Who are the competitors of {product}, if you dont know the answer give a plausible answer'
+    competitors: 'Who are the competitors of {product}, please provide a list of the specific competitor names and a description of the company if you know it, if you dont know the answer give a plausible answer'
 }
